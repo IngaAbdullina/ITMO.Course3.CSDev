69bcca9c31710db91fb8e9f487042f3a7c61aa2d
IngaAbdullina <95912864+IngaAbdullina@users.noreply.github.com>
2022-05-17 15:19:47 +0300

    Create Task1.cs

---
+++ Lab1.Task1/Task1.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ITMO.Course3.Lab1
+{
+    internal class Task1
+    {
+        public static void Main(string[] args)
+        {
+            string myName;
+            Console.WriteLine("Please enter your name");
+            myName = Console.ReadLine( );
+            Console.WriteLine("Hello {0}", myName); 
+        }
+    }
+}
