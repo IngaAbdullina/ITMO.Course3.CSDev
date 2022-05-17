8443aa56e7cd3de679697c7af6138cea85cd1da2
IngaAbdullina <95912864+IngaAbdullina@users.noreply.github.com>
2022-05-17 16:58:34 +0300

    Add files via upload

---
+++ Lab2/Lab2.Task2/Task2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lab2.Task2
+{
+    public enum AccountType { Checking, Deposit }
+
+    public struct BankAccount
+    {
+        public long accNo;
+        public decimal accBal;
+        public AccountType accType;
+    }
+    
+    internal class Task2
+    {
+        public static void Main(string[] args)
+        {
+            BankAccount goldAccount;
+            goldAccount.accType = AccountType.Checking;
+            goldAccount.accBal = (decimal)3200.00;
+            goldAccount.accNo = 123;
+
+            Console.WriteLine("*** Account Summary ***");
+            Console.WriteLine("Account Number {0}", goldAccount.accNo);
+            Console.WriteLine("Account Type {0}", goldAccount.accType);
+            Console.WriteLine("Account Balance ${0}",goldAccount.accBal);
+
+        }
+    }
+}
