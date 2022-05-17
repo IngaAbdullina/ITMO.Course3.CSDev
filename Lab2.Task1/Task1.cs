03e18214f67bcdd4f6ff86bf894332b2083c431d
IngaAbdullina <95912864+IngaAbdullina@users.noreply.github.com>
2022-05-17 16:28:35 +0300

    Add files via upload

---
+++ Lab2.Task1/Task1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab2.Task1
+{
+    public enum AccountType { Checking, Deposit }
+    
+    internal class Task1
+    {
+        public static void Main(string[] args)
+        {
+            AccountType goldAccount = AccountType.Checking;
+            AccountType platinumAccount = AccountType.Deposit;
+            
+            Console.WriteLine("The Customer Account Type is {0}", goldAccount);
+            Console.WriteLine("The Customer Account Type is {0}", platinumAccount);
+        }
+    }
+}
