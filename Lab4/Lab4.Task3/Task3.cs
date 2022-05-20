6b8276178a54eb36cefb48e80dbb37ef0d586d8c
IngaAbdullina <95912864+IngaAbdullina@users.noreply.github.com>
2022-05-20 20:23:54 +0300

    Add files via upload

--- Lab4/Lab4.Task3/Task3.cs
+++ Lab4/Lab4.Task3/Task3.cs
@@ -17,9 +17,21 @@ namespace Lab4
 
         private static int Factorial(int n)
         {
-            if (n == 1) return 1;
- 
-            return n * Factorial(n - 1);
+            if (n < 0) return -1;
+            if (n == 0 || n == 1) return 1;
+            
+            try
+            {
+                checked
+                {
+                    return n * Factorial(n - 1);
+                }
+            }
+            catch (Exception caught) 
+            {
+                Console.WriteLine(caught);
+                return -1;
+            }
         }
     }
 }
