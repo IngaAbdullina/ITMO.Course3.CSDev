1113380a1ad0751557093ccc2ae5e1166419d454
IngaAbdullina <95912864+IngaAbdullina@users.noreply.github.com>
2022-05-29 17:59:53 +0300

    Add files via upload

---
+++ ITMO.Course3.CSDev.Lab8/Lab8.Task1/AccountType.cs
@@ -0,0 +1,7 @@
+namespace Lab8
+{
+    public enum AccountType
+    {
+        Checking, Deposit
+    }
+}
---
+++ ITMO.Course3.CSDev.Lab8/Lab8.Task1/MainClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Lab8
+{
+    internal class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount(AccountType.Deposit, 500);
+            Write(account);
+        }
+
+        static void Write(BankAccount toWrite)
+        {
+            Console.WriteLine("Account number is {0}", toWrite.Number());
+            Console.WriteLine("Account balance is {0}", toWrite.Balance());
+            Console.WriteLine("Account type is {0}", toWrite.Type());
+        }
+        
+        public static void TestDeposit(BankAccount acc)
+        {
+            Console.Write("Enter amount to deposit: ");
+            decimal amount = decimal.Parse(Console.ReadLine());
+            acc.Deposit(amount);
+        }
+        
+        public static void TestWithdraw(BankAccount acc)
+        {
+            Console.Write("Enter amount to withdraw: ");
+            decimal amount = decimal.Parse(Console.ReadLine());
+            if (!acc.Withdraw(amount)) {
+                Console.WriteLine("Insufficient funds.");
+            }
+        }
+    }
+}
