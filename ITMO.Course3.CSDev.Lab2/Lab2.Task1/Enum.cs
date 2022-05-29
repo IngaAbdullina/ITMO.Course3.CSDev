ebcc27fbce6fb539941a35a991c6cbe15116b419
IngaAbdullina <95912864+IngaAbdullina@users.noreply.github.com>
2022-05-29 14:37:34 +0300

    Add files via upload

---
+++ ITMO.Course3.CSDev.Lab2/Lab2.Task1/Enum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab2
+{
+    public enum AccountType { Checking, Deposit }
+    
+    internal class Enum
+    {
+        public static void Main(string[] args)
+        {
+            AccountType goldAccount = AccountType.Checking;
+            AccountType platinumAccount = AccountType.Deposit;
+            
+            Console.WriteLine("The Customer Account Type is {0}", goldAccount);
+            Console.WriteLine("The Customer Account Type is {0}", platinumAccount);
+        }
+    }
+}
